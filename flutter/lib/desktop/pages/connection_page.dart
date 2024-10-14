c19a3de6160c80d2574ba0b1b9ebde3360753115
魚丸包粗麵 <6185852@qq.com>
2024-10-14 20:03:14 +0800

    connection_page.dart

--- flutter/lib/desktop/pages/connection_page.dart
+++ flutter/lib/desktop/pages/connection_page.dart
@@ -39,7 +39,7 @@ class _OnlineStatusWidgetState extends State<OnlineStatusWidget> {
   double? get height => bind.isIncomingOnly() ? null : em * 3;
 
   void onUsePublicServerGuide() {
-    const url = "https://remote.naeeo.com/pricing";
+    const url = "https://remote.naeeo.com/description";
     canLaunchUrlString(url).then((can) {
       if (can) {
         launchUrlString(url);
